b5206c39abbb2a3fa9b754de7d0f5e3e0d6d54b9
Nikunj Aggarwal <nikunj.aggarwal@monotype.com>
2017-05-03 10:22:27 +0530

    Minor final changes before deleting project from local

--- auto-test-executor/src/main/java/com/auto/solution/TestExecutor/TestRunner.java
+++ auto-test-executor/src/main/java/com/auto/solution/TestExecutor/TestRunner.java
@@ -9,34 +9,36 @@ import com.auto.solution.TestLogging.TestLogger;
 
 public class TestRunner {
 
-	private String getProjectBasePath(){
-		String projectBasePath = new File(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent()).getParent();
+	private String getProjectBasePath() {
+		String projectBasePath = new File(
+				new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent())
+						.getParent();
 		return projectBasePath;
 	}
+
 	public static void main(String[] args) throws Exception {
-		try{
+		try {
 			TestRunner runner = new TestRunner();
-			
-			TestEngineHelper helper = new TestEngineHelper();	
-			
+
+			TestEngineHelper helper = new TestEngineHelper();
+
 			TestEngine engine = new TestEngine(helper);
-			
+
 			engine.prepareTestEngineForExecution(runner.getProjectBasePath());
-			
+
 			engine.initiateExecution();
-			
+
 			engine.logExecutionDetailsIntoXml();
-			
+
 			engine.initateReportingOfTestExecutionDetails();
-			
-			System.out.println(engine.isAnyTestStepFailedDuringTestExecution());
-			
-			if(engine.isAnyTestStepFailedDuringTestExecution()){
+
+			System.out.println("Any test step failed - " + engine.isAnyTestStepFailedDuringTestExecution());
+
+			if (engine.isAnyTestStepFailedDuringTestExecution()) {
 				TestLogger.getInstance(null).ERROR(Property.ERROR_MESSAGES.TEST_FAILURE.getErrorMessage());
 				throw new Exception(Property.ERROR_MESSAGES.TEST_FAILURE.getErrorMessage());
-		}		
-		}
-		catch(Exception e){
+			}
+		} catch (Exception e) {
 			throw e;
 		}
 
